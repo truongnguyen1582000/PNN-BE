6cafbd3a5b995aa54774d8c759b432606b2d2355
truongnguyen1582000 <truongnguyen1582000>
2022-04-14 15:26:38 +0700

    make it better

--- routes/messages.js
+++ routes/messages.js
@@ -1,29 +1,30 @@
-const router = require("express").Router();
-const Message = require("../models/Message");
+const router = require('express').Router();
+const Message = require('../models/Message');
 
 //add
-router.post("/", async (req, res) => {
-    const newMessage = new Message(req.body)
+router.post('/', async (req, res) => {
+  const newMessage = new Message(req.body);
 
-    try {
-        const savedMessage = await newMessage.save();
-        res.status(200).json(savedMessage);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// /here
 
 //get
-router.get("/:conversationId", async (req, res) => {
-    try {
-        const messages = await Message.find({
-            conversationId: req.params.conversationId,
-        });
-        res.status(200).json(messages);
-
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.get('/:conversationId', async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
